6889c128747a31d9349442ad71021cfefc847ab1
MtAleph <riwa.hoteit@gmail.com>
2021-04-07 20:59:01 +0300

    Update Bot.java

--- src/main/java/com/riwa/Bot.java
+++ src/main/java/com/riwa/Bot.java
@@ -10,8 +10,8 @@ import discord4j.core.object.presence.Activity;
 import discord4j.core.object.presence.Presence;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.message.MessageCreateEvent;
-import discord4j.core.object.entity.Message;
 import discord4j.core.object.reaction.ReactionEmoji;
+import discord4j.core.object.entity.Message;
 import discord4j.core.object.entity.channel.MessageChannel;
 import discord4j.rest.util.Color;
 
